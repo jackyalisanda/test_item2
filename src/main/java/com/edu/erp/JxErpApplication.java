78187306fdfaeef6a881e8371871de17a7a43645
tinako <2513153913@qq.com>
2026-05-27 20:58:19 +0800

    commit no.4： JxErpApplication.java

--- src/main/java/com/edu/erp/JxErpApplication.java
+++ src/main/java/com/edu/erp/JxErpApplication.java
@@ -3,5 +3,6 @@ package com.edu.erp;
 public class JxErpApplication {
     public static void main(String[] args) {
         System.out.println("JxErpApplication hello100");
+        System.out.println("JxErpApplication hello200 for v3.o");
     }
 }
